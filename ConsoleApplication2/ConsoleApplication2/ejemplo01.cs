9bcf365cb659acf93398942423dcb1b795ba410b
isela <isela.ms18@outlook.com>
2016-06-20 11:25:40 -0500

    editado y eliinado

--- ConsoleApplication2/ConsoleApplication2/ejemplo01.cs
+++ ConsoleApplication2/ConsoleApplication2/ejemplo01.cs
@@ -11,8 +11,7 @@ namespace ConsoleApplication2
         static void Main(string[] args)
         {
             string  "cadena",
-                int 1=2,
-            public 
+                           public 
 
         }
     }
